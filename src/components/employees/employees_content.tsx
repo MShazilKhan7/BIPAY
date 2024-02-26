dda014ef39a4938f37777d6706ff0138b6f65e0f
MShazilKhan7 <shazilk14@gmail.com>
2024-02-26 17:07:52 +0500

    backend

--- src/components/employees/employees_content.tsx
+++ src/components/employees/employees_content.tsx
@@ -9,9 +9,11 @@ interface Employee {
   profile: string; 
   id: number;
   status: string;
-  Department: string;
+  department: string;
   shift: string;
-  Role: string;
+  role: string;
+  joining_date: string;
+
 }
 
 interface TableData {
@@ -32,12 +34,13 @@ function EmployeesContent():any{
               const response = await axios.get('http://127.0.0.1:8000/api/employees/');
               const responseData = response.data;
               const updatedEmployees = responseData.map((employeeData:any) => ({
-                  profile: employeeData.first_name + employeeData.last_name,
+                  profile: employeeData.first_name +" "+ employeeData.last_name,
                   id: employeeData.id,
-                  shift: employeeData.shift,
                   status: employeeData.status,
-                  Department: employeeData.department_emp.name,
-                  Role: employeeData.designation_emp
+                  department: employeeData.department_emp.name,
+                  shift: employeeData.shift,
+                  joining_date: employeeData.joining_date,
+                  role: employeeData.designation_emp.name,
               }));
   
               // Update the employees state with the new array of employees
@@ -54,7 +57,7 @@ function EmployeesContent():any{
 
     // i can store this into the redux store and take the data from that state directly inside the table...
     let data: TableData = {
-        tableHeader : ['profile','id','status','Department','shift', 'Role'],
+        tableHeader : ['profile','id','status','Department','shift', 'Joining Date','Role'],
         tableData   : employees
     }
     return (
